e2d25fd1d066a4488db56b20e9cb84515392e821
Darina Yordanova <darinayordanova-itsoft>
2021-01-08 11:31:11 +0200

    Updates

---
+++ precache-manifest.c15ba83d0864b432b090f21cc43938ec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d178d44f1577477e574f79a0a5fdf7",
+    "url": "/ToDo-List-with-React/index.html"
+  },
+  {
+    "revision": "180fd5e5137ccdeef930",
+    "url": "/ToDo-List-with-React/static/css/main.9d7694a3.chunk.css"
+  },
+  {
+    "revision": "516cda1ffe01e2c33d8b",
+    "url": "/ToDo-List-with-React/static/js/2.aa81dc4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List-with-React/static/js/2.aa81dc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180fd5e5137ccdeef930",
+    "url": "/ToDo-List-with-React/static/js/main.ac3035bd.chunk.js"
+  },
+  {
+    "revision": "4864e8a59ca9528ccc58",
+    "url": "/ToDo-List-with-React/static/js/runtime-main.a72ca842.js"
+  },
+  {
+    "revision": "4ef8f6f2f0d0e2d34c59c9e604366024",
+    "url": "/ToDo-List-with-React/static/media/Logo.4ef8f6f2.svg"
+  },
+  {
+    "revision": "5c3430d6479bae81b407653465706d5f",
+    "url": "/ToDo-List-with-React/static/media/add-white.5c3430d6.svg"
+  },
+  {
+    "revision": "10fc5d30a02ca000ea6e309764b0d6cf",
+    "url": "/ToDo-List-with-React/static/media/body.10fc5d30.svg"
+  },
+  {
+    "revision": "951f9b19e75a9aed0821566c52aa91a6",
+    "url": "/ToDo-List-with-React/static/media/checked.951f9b19.svg"
+  },
+  {
+    "revision": "af905a93d8ba4835caaffe64847b0a35",
+    "url": "/ToDo-List-with-React/static/media/clock.af905a93.svg"
+  },
+  {
+    "revision": "a3efe2ef9680dfee88b03800e0036512",
+    "url": "/ToDo-List-with-React/static/media/close.a3efe2ef.svg"
+  },
+  {
+    "revision": "4cb9e625979dc9c7d836b63e40382efc",
+    "url": "/ToDo-List-with-React/static/media/options.4cb9e625.svg"
+  },
+  {
+    "revision": "ef23e5fe23d76faa3d9e946b9eaa691d",
+    "url": "/ToDo-List-with-React/static/media/unchecked.ef23e5fe.svg"
+  }
+]);
